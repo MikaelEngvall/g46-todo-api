82f3df9c48dd9c91e95254fc695be3702802a24b
MikaelEngvall <mikael@engvall.org>
2023-11-02 21:33:52 +0100

    Changes by kaarelvisnapuu

--- src/main/java/se/lexicon/g46todoapi/repository/UserRepository.java
+++ src/main/java/se/lexicon/g46todoapi/repository/UserRepository.java
@@ -1,21 +1,20 @@
 package se.lexicon.g46todoapi.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g46todoapi.domain.entity.User;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     Boolean existsByEmail(String email);
 
+    User getUserByEmail(String email);
+
     @Modifying
-    @Transactional
     @Query("update User u set u.expired = :status where u.email = :email")
     void updateExpiredByEmail(@Param("email") String email, @Param("status") boolean status);
 
--- src/test/java/se/lexicon/g46todoapi/repository/UserRepositoryTest.java
+++ src/test/java/se/lexicon/g46todoapi/repository/UserRepositoryTest.java
@@ -5,37 +5,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.Modifying;
-import se.lexicon.g46todoapi.domain.entity.Role;
+import org.springframework.test.annotation.Rollback;
 import se.lexicon.g46todoapi.domain.entity.User;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@Transactional
 class UserRepositoryTest {
 
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleRepository;
+    private UserRepository sut;
 
     @Test
-    @Modifying
+    @Transactional
+    @Rollback(false)
     void testUpdateExpiredByEmail() {
         // Create a new user and save it to the database.
         User userOne = new User("test@test.com", "1234");
         userOne.setExpired(false);
-        userRepository.save(userOne);
+        sut.save(userOne);
 
-        // Update the expired value of the user. So I hoped but I dunno if it is actually happening
-        userRepository.updateExpiredByEmail("test@test.com", true);
+        // Update the expired value of the user.
 
-        // Get the user from the db
-        User updatedUser = userRepository.findById("test@test.com").orElse(null);
 
-        // Assert that the expired value of the user is now true. But it isn't!!!!!
-        assertThat(updatedUser.isExpired()).isTrue();
+        // Refresh the user from the database.
+        sut.updateExpiredByEmail("test@test.com", true);  // Not working, why? TODO
+
+        // Assert that the expired value of the user is now true.
+        assertTrue(sut.getUserByEmail("test@test.com").isExpired());
     }
 
 }
